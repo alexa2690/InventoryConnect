2c1ece78ee8bd5a6ac51362fbdca8702d8363101
ipacbalaj <ipacbalaj@gmail.com>
2016-03-21 15:39:39 +0200

    add_requsition finished

--- src/app/components/editTableDirective/editTableDirective.js
+++ src/app/components/editTableDirective/editTableDirective.js
@@ -13,17 +13,25 @@
     		restrict : 'E',
     		templateUrl :'app/components/editTableDirective/editTableDirective.html',
     		scope : {
-    			itemsList :'='
-                //todo:pass add and delete methods from controller
+    			itemsList :'=',
+                dropdownItems : '='
     		},
             link : function($scope,$element, $attrs, $controller){
 
                     //scope methods
                     //todo: call controller method
+                    $scope.showAddRow = false;                    
+                    $scope.currentItem = {};
+                    
 
                     $scope.addRequisitionItem = function(){
                         //todo: call controller method
-                        $scope.itemsList.push(new RequisitionItem('itemName','unitName',5,4))
+                        if($scope.showAddRow){
+                            var newItem = angular.copy($scope.currentItem.selected);
+                            $scope.itemsList.push(newItem)
+                        }
+                        $scope.currentItem = {};
+                        $scope.showAddRow = true;
                     }
 
                     $scope.removeRequisitionItem = function(reqItem){
@@ -32,12 +40,36 @@
                         $scope.itemsList.splice(index,1);
                     }
 
+                    $scope.cancelAddItem = function(){
+                        $scope.currentItem = {};
+                        $scope.showAddRow = false;
+                    }
+
+                    $scope.$watch('currItemId',function(){
+
+                        console.log($scope.currItemId);
+                    })
+
                     function RequisitionItem(name,unit,par,qty){
                         this.Name = name;
                         this.Unit = unit;
                         this.Par = par;
                         this.Quantity = qty;
-            } 
+                    }
+
+                    //TESTING
+
+                      $scope.person = {};
+                      $scope.people = [
+                        { name: 'Adam',      email: 'adam@email.com',      age: 10 },
+                        { name: 'Amalie',    email: 'amalie@email.com',    age: 12 },
+                        { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
+                        { name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
+                        { name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
+                        { name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
+                        { name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
+                        { name: 'Adrian',    email: 'adrian@email.com',    age: 21 }
+                      ]; 
 
       
             }
